c64f2f8e7edb86aedbc6e69d725c15d61050dd2f
Manish Kumar Singh <101065330+manishksingh441@users.noreply.github.com>
2026-03-02 01:56:00 +0530

    Create my_Python.py

---
+++ my_Python.py
@@ -0,0 +1,33 @@
+a = input("a")
+b = input("b")
+sum = int(a)+int(b)
+print(sum)
+tuple = (a, b, sum)
+print(tuple)
+tup = (10, 34, 123)
+print(tup)
+# print(type(tup))
+tuple = (1, 1, 3, 4, 5)
+print(tuple.index(3))
+print(tuple.count(1))
+movies = []
+mov1 = input("Enter first movie: ")
+mov2 = input("Enter second movie: ")
+mov3 = input("Enter third movie: ")
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+movies.append(input("Enter 4th movie: "))
+print(movies)
+
+list1 = [1, 2, 3]
+copy_list1 = list1.copy()
+copy_list1.reverse()
+if (copy_list1 == list1):
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+print(copy_list1)
+grade = ["Grade A", "Grade B", "Grade A", "Grade D", "Grade A", "Grade C"]
+grade.sort()
+print(grade)
